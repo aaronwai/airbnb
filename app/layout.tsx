e54ac9c975d6ab09aba492ed065d3744c6c582ec
aaronwai <aaron.lung.wai@gmail.com>
2024-01-16 23:07:57 +0800

    Step 7 : registger form

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
-import Modal from "./components/modals/Modal";
+import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
 export const metadata = {
   title: "Airbnb",
   description: "Airbnb",
@@ -19,7 +20,8 @@ export default function RootLayout({
     <html lang='en'>
       <body className={font.className}>
         <ClientOnly>
-          <Modal isOpen title='Login' actionLabel='Submit' />
+          <ToasterProvider />
+          <RegisterModal />
           <Navbar />
         </ClientOnly>
         {children}
