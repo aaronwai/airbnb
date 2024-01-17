5034e6c70f3d3d48f3eeae661c06895fee98fb71
aaronwai <aaron.lung.wai@gmail.com>
2024-01-17 18:22:41 +0800

    Step 13 : RentalModal I

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -13,9 +13,11 @@ import Input from "../input/Input";
 import toast from "react-hot-toast";
 import Button from "../Button";
 import { signIn } from "next-auth/react";
+import useLoginModal from "@/app/hooks/useLoginModal";
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
   const [isLoading, setIsLoading] = useState(false);
 
   const {
@@ -45,6 +47,11 @@ const RegisterModal = () => {
         setIsLoading(false);
       });
   };
+
+  const onToggle = useCallback(() => {
+    registerModal.onClose();
+    loginModal.onOpen();
+  }, [registerModal, loginModal]);
 
   const bodyContent = (
     <div className='flex flex-col gap-4'>
@@ -103,7 +110,7 @@ const RegisterModal = () => {
         <p>
           Already have an account?
           <span
-            onClick={registerModal.onClose}
+            onClick={onToggle}
             className='
               text-neutral-800
               cursor-pointer 
---
+++ app/components/modals/RentModal.tsx
@@ -0,0 +1,67 @@
+"use client";
+import { useState, useMemo } from "react";
+import useRentModal from "@/app/hooks/useRentModal";
+
+import Modal from "./Modal";
+import Heading from "../Heading";
+
+enum STEPS {
+  CATEGORY = 0,
+  LOCATION = 1,
+  INFO = 2,
+  IMAGES = 3,
+  DESCRIPTION = 4,
+  PRICE = 5,
+}
+
+const RentModal = () => {
+  const rentModal = useRentModal();
+  // define steps and action labels
+  const [step, setStep] = useState(STEPS.CATEGORY);
+  const onBack = () => {
+    setStep((value) => value - 1);
+  };
+
+  const onNext = () => {
+    setStep((value) => value + 1);
+  };
+
+  const actionLabel = useMemo(() => {
+    if (step === STEPS.PRICE) {
+      return "Create";
+    }
+
+    return "Next";
+  }, [step]);
+
+  const secondaryActionLabel = useMemo(() => {
+    if (step === STEPS.CATEGORY) {
+      return undefined;
+    }
+
+    return "Back";
+  }, [step]);
+
+  let bodyContent = (
+    <div className='flex flex-col gap-8'>
+      <Heading
+        title='Which of these best describes your place?'
+        subtitle='Pick a category'
+      />
+    </div>
+  );
+  return (
+    <Modal
+      isOpen={rentModal.isOpen}
+      title='Airbnb your home!'
+      actionLabel={actionLabel}
+      secondaryActionLabel={secondaryActionLabel}
+      secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
+      onSubmit={rentModal.onClose}
+      onClose={rentModal.onClose}
+      body={bodyContent}
+    />
+  );
+};
+
+export default RentModal;
