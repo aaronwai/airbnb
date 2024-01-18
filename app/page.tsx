449dfe6c2f7ee7a5c3f322dde1b1bab77281d32e
aaronwai <aaron.lung.wai@gmail.com>
2024-01-18 15:53:08 +0800

    Step 19 : Listing I

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,36 @@
+import Container from "@/app/components/Container";
+
+import ClientOnly from "./components/ClientOnly";
+import EmptyState from "./components/EmptyState";
+
 export default function Home() {
-  return <div className='text-rose-500 text-2xl'>Hello</div>;
+  const isEmpty = true;
+  if (isEmpty) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+  return (
+    <ClientOnly>
+      <Container>
+        <div
+          className='
+            pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          '
+        >
+          my listing
+        </div>
+      </Container>
+    </ClientOnly>
+  );
 }
